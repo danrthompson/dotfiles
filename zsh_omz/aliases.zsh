cf59143f6ceda828f5b9f51aa51dcda1854768cb
Dan Thompson <danielrthompsonjr@gmail.com>
2023-10-27 15:45:01 -0400

    changes to dotenv

--- zsh_omz/aliases.zsh
+++ zsh_omz/aliases.zsh
@@ -26,23 +26,15 @@ alias install='nocorrect install'
 ### Git
 alias gs='git status'
 alias gc='git commit -a -m'
+alias gom='git checkout main'
+
+### Pyenv
+alias pd='pyenv deactivate'
 
 ### Chatblade
 alias -g cb='chatblade'
 
-### fasd
-# dirs
-alias jd='fasd -d'
-# files
-alias jf='fasd -f'
-# files and dirs
-alias ja='fasd -a'
-# dirs with scores, interactive
-alias jjd='fasd -sid'
-# files with scores, interactive
-alias jjf='fasd -sif'
-# paths with scores, interactive mode
-alias jja='fasd -si'
+### fasd - recent (or just past) directory and file navigation, in iTerm and VSCode or Finder
 # function to execute built-in cd
 fasd_cd() {
   if [ $# -le 1 ]; then
@@ -53,31 +45,103 @@ fasd_cd() {
     [ -d "$_fasd_ret" ] && cd "$_fasd_ret" || printf %s\n "$_fasd_ret"
   fi
 }
+
+## Opening directories in iTerm. Provide a search string and it will find the best matching directories and open the best (or chosen) one in iTerm
+# Open the best matching dir
 alias j='fasd_cd -d'
+# Interactively choose among the best matching directories to open in iTerm
 alias jj='fasd_cd -d -i'
 
-alias jc='fasd -a -e code'
-alias jjc='fasd -sia -e code'
+## Opening directories in VSCode
+# Open best match
+alias jc='fasd -d -e code'
+# Interactively select among best matches
+alias jjc='fasd -sid -e code'
+
+## Opening files in VSCode
+# Open best match
+alias jcf='fasd -f -e code'
+# Interactively select among best matches
+alias jjcf='fasd -sif -e code'
+
+## Opening directories in Finder
+# Open best match
+alias jo='fasd -d -e open'
+# Interactively select among best matches
+alias jjo='fasd -sid -e open'
+
+## Opening files in Finder
+# Open best match
+alias jof='fasd -f -e open'
+# Interactively select among best matches
+alias jjof='fasd -sif -e open'
+
+# Show all matching dirs with scores (optionally provide a search string)
+alias jds='fasd -d'
+# Ditto for files
+alias jfs='fasd -f'
+# Ditto for files and dirs
+alias jas='fasd -a'
+# Dirs and scores with interactive selection - not sure what this is for actually. Disabling until I figure that out. Maybe could be used to open the file in another app like Finder or VSCode?
+# alias jjd='fasd -sid'
+# # files with scores, interactive
+# alias jjf='fasd -sif'
+# # paths with scores, interactive mode
+# alias jja='fasd -si'
 
 ## Dir and file shortcuts
-alias cdst='cd ~/Code/stock_price_prediction/'
-alias zshrc="code /Users/danthompson/Code/dotfiles/zsh_omz"
-alias jn='jupyter-notebook ~/Code/Notebooks'
+# Work dirs
+# Dictation app
+alias oww='cd /Users/danthompson/Code/Git/XCode/WhisperDictationApp'
+alias cww='code /Users/danthompson/Code/Git/XCode/WhisperDictationApp'
+# OnlyFans app
+alias oof='cd /Users/danthompson/Code/Git/my_repos/OnlyFans'
+alias cof='code /Users/danthompson/Code/Git/my_repos/OnlyFans'
+
+# Useful common dirs
+# alias cdst='cd ~/Code/stock_price_prediction/'
+alias zz="code /Users/danthompson/Code/dotfiles"
+alias pynb='jupyter-notebook ~/Code/Notebooks'
+
 
 ## Command aliases
 alias lsr='ls -lARFh'
 alias h='history'
 alias p='ps -f'
-alias H='\| head'
 alias ipy='ipython'
 alias or="omz reload"
 
+## Pipe commands
+alias -g qh='| head '
+alias -g qt='| tail '
+alias -g qtf='| tail -f '
+alias -g ql='| wc -l '
+alias -g qg='| grep '
+alias -g pa='pyenv activate '
+
 ## Environment management
 alias pybook='cd /Users/danthompson/Code/Courses/pydata-book; mamba activate pydata-book'
-
+# activate data_science_env managed by conda/mamba
+alias ads='mamba activate data_science_env'
 
 # global aliases
 ## Main program override
 alias -g e="exa -a"
 alias -g cat="bat -p"
 alias -g b="bat"
+alias -g ggs="sgpt"
+alias cc="it2copy"
+
+alias ww="~/Code/Tools/cloned/whisper-writer/src/start_background_process.zsh"
+
+alias wk="~/Code/Tools/cloned/whisper-writer/src/kill_background_process.zsh"
+alias wl="tail -f ~/Code/Tools/cloned/whisper-writer/nohup.out"
+
+alias wr='if [ -n "$(ps aux | grep "src/km_handler" | grep -v grep)" ]; then echo "Process is running"; else echo "Process is not running"; fi'
+
+alias wf="osascript -e 'tell application \"Keyboard Maestro Engine\" to setvariable \"WhisperRunning\" to \"no\"'"
+
+function ggr() {
+  cd /Users/danthompson/Code/Tools/cloned/gpt-researcher
+  uvicorn main:app --reload
+}
--- zsh_omz/package_plugin_setup.zsh
+++ zsh_omz/package_plugin_setup.zsh
@@ -1,3 +1,10 @@
+export AWS_DEFAULT_PROFILE=admin
+
+# completions
+autoload -U bashcompinit
+bashcompinit
+
+
 # vscode
 ## Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
@@ -6,12 +13,10 @@ else
     export EDITOR='nano'
 fi
 
-# goku setup
-export GOKU_EDN_CONFIG_FILE="/Users/danthompson/Dropbox/karabiner/karabiner.edn"
 
 # conda
-## >>> conda initialize >>>
-## !! Contents within this block are managed by 'conda init' !!
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
 __conda_setup="$('/Users/danthompson/mambaforge/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
@@ -23,6 +28,7 @@ else
     fi
 fi
 unset __conda_setup
+# <<< conda initialize <<<
 
 if [ -f "/Users/danthompson/mambaforge/etc/profile.d/mamba.sh" ]; then
     . "/Users/danthompson/mambaforge/etc/profile.d/mamba.sh"
@@ -30,9 +36,6 @@ fi
 ## <<< conda initialize <<<
 
 
-# mcfly
-eval "$(mcfly init zsh)"
-export MCFLY_FUZZY=2
 
 # deta
 ## export PATH="/Users/danthompson/.deta/bin:$PATH"
@@ -42,20 +45,15 @@ fpath=(~/.zsh.d/ $fpath)
 # iTerm
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-## completion
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
-# fzf
-source /opt/homebrew/share/antigen/antigen.zsh [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
 # antigen
 source /opt/homebrew/share/antigen/antigen.zsh
 
-# p10k theme
-## To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+# fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+
+# # p10k theme
+# ## To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 
@@ -97,6 +95,7 @@ export BAT_THEME="Dracula"
 export PAGER=most
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
+
 # fasd
 eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install \
     zsh-wcomp zsh-wcomp-install)" >> "/dev/null" 2>&1
@@ -105,7 +104,7 @@ eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install \
 # check if fasd is installed
 ## setup
 fasd_cache="${ZSH_CACHE_DIR}fasd-init-cache"
-if [[ "$commands[fasd]" -nt "$fasd_cache" || ! -s "$fasd_cache" ]]; then
+if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
   fasd --init zsh-hook zsh-ccomp zsh-ccomp-install \
     zsh-wcomp zsh-wcomp-install >| "$fasd_cache"
 fi
@@ -116,5 +115,46 @@ unset fasd_cache
 _FASD_MAX=5000
 _FASD_BACKENDS="native current spotlight"
 
-# direnv
-eval "$(direnv hook zsh)"
+# Shell-GPT integration ZSH v0.1
+_sgpt_zsh() {
+if [[ -n "$BUFFER" ]]; then
+    _sgpt_prev_cmd=$BUFFER
+    BUFFER+="⌛"
+    zle -I && zle redisplay
+    BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd")
+    zle end-of-line
+fi
+}
+zle -N _sgpt_zsh
+bindkey ^l _sgpt_zsh
+# Shell-GPT integration ZSH v0.1
+
+eval "$(fnm env --use-on-cd)"
+
+
+async_init
+
+# Async load slow things
+function async_load_slow_packages() {
+    # pyenv
+    eval "$(command pyenv init -)"
+    eval "$(command pyenv virtualenv-init -)"
+
+    # mcfly
+    eval "$(mcfly init zsh)"
+    # direnv
+    eval "$(direnv hook zsh)"
+
+    # The next line updates PATH for the Google Cloud SDK.
+    if [ -f '/Users/danthompson/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/danthompson/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+    # The next line enables shell command completion for gcloud.
+    if [ -f '/Users/danthompson/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/danthompson/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
+}
+
+async_start_worker slow_packages_worker -n
+async_register_callback slow_packages_worker async_load_slow_packages
+async_job slow_packages_worker async_load_slow_packages
+
+# fast stuff from async load
+export MCFLY_FUZZY=2
+export NVM_DIR="$HOME/.nvm"
